a6a4e0dafed4fc2e15561bac5f0cfd0474abce3f
Afan Dayu L <afandayul@gmail.com>
2024-03-21 00:23:27 +0700

    feat: init security

---
+++ src/main/java/com/tradesphere/app/security/AuthTokenFilter.java
@@ -0,0 +1,53 @@
+package com.tradesphere.app.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try{
+            String headerAuth = request.getHeader(HttpHeaders.AUTHORIZATION);
+            String token = null;
+            if(headerAuth != null && headerAuth.startsWith("Bearer ")) {
+                token = headerAuth.substring(7);
+            }
+
+            if(token != null && jwtUtil.verifyJwtToken(token)) {
+                Map<String,String> userInfo = jwtUtil.getUserInfoByToken(token);
+                UserDetails user = userService.loadUserByUserId(userInfo.get("userId"));
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource());
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }catch (Exception e) {
+            e.getMessage();
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/tradesphere/app/security/JwtUtil.java
@@ -0,0 +1,56 @@
+package com.tradesphere.app.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+    @Value("${app.enigmacamp.jwt.jwt-secret}")
+    private String jwtSecret;
+    @Value("${app.enigmacamp.jwt.app-name}")
+    private String appName;
+    @Value("${app.enigmacamp.jwt.jwt-expired}")
+    private long jwtExpiration;
+    public String generateToken(AppUser appUser) {
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        return JWT.create()
+                .withIssuer(appName)
+                .withSubject(appUser.getId())
+                .withExpiresAt(Instant.now().plusSeconds(jwtExpiration))
+                .withIssuedAt(Instant.now())
+                .withClaim("role", appUser.getRole().name())
+                .sign(algorithm);
+    }
+
+    public boolean verifyJwtToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = verifier.verify(token);
+        return  decodedJWT.getIssuer().equals(appName);
+    }
+
+    public Map<String, String> getUserInfoByToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes(StandardCharsets.UTF_8));
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+
+            Map<String, String> userinfo = new HashMap<>();
+            userinfo.put("userId", decodedJWT.getSubject());
+            userinfo.put("role", decodedJWT.getClaim("role").asString());
+
+            return userinfo;
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException();
+        }
+    }
+
+}
